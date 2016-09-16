6dcd0ffb34637223eb87bb6bb29ff7eae5a5cd81
Alexsander Akers <me@a2.io>
2016-09-16 20:28:14 +0200

    Initial commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,14 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    let lang = req.headers["Accept-Language"]?.string ?? "en"
+    return try drop.view.make("welcome", [
+    	"message": Node.string(drop.localization[lang, "welcome", "title"])
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
