486b8231f2651495a3de5965a087bb3f683fd57b
Alexsander Akers <me@a2.io>
2016-09-22 10:43:35 +0200

    Update Kitura and HeliumLogger versions

--- Package.swift
+++ Package.swift
@@ -3,16 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Passcards",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 17),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0),
         .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 1),
-    ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-        "Tests",
     ]
 )
