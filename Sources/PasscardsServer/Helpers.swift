12e109d437cfc487e62200d4b738e4fc14f42a29
Alexsander Akers <me@a2.io>
2016-10-03 16:03:05 +0200

    Remove unused makeId function

--- Sources/PasscardsServer/Helpers.swift
+++ Sources/PasscardsServer/Helpers.swift
@@ -17,25 +17,6 @@ let iso8601DateFormatter: DateFormatter = {
     return dateFormatter
 }()
 
-func makeId(length: Int = 10) -> String {
-    var result = ""
-    for _ in 0 ..< length {
-        let value = arc4random_uniform(62)
-        switch value {
-        case 0 ..< 26:
-            result += String(UnicodeScalar(0x41 + UInt8(value)))
-        case 26 ..< 52:
-            result += String(UnicodeScalar(0x61 + UInt8(value - 26)))
-        case 52 ..< 62:
-            result += String(UnicodeScalar(0x30 + UInt8(value - 52)))
-        default:
-            preconditionFailure()
-        }
-    }
-
-    return result
-}
-
 func parseVanityId(from fileName: String) -> String? {
     guard fileName.lowercased().hasSuffix(".pkpass"),
         let extensionIndex = fileName.characters.index(of: "."),
